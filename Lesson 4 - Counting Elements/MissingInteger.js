7dd926fe1cfef45d901f0f001c0c13c06a6dc356
Vladan Petrovic <vladan.petrovic@comtrade.com>
2017-04-24 14:32:31 +0200

    Added MissingInteger

---
+++ Lesson 4 - Counting Elements/MissingInteger.js
@@ -0,0 +1,43 @@
+/**
+ * Write a function:
+ *
+ * function solution(A);
+ * that, given a non-empty zero-indexed array A of N integers, returns the minimal positive integer (greater than 0) that does not occur in A.
+ *
+ * For example, given:
+ *
+ * A[0] = 1
+ * A[1] = 3
+ * A[2] = 6
+ * A[3] = 4
+ * A[4] = 1
+ * A[5] = 2
+ * the function should return 5.
+ *
+ * Assume that:
+ *
+ * N is an integer within the range [1..100,000];
+ * each element of array A is an integer within the range [−2,147,483,648..2,147,483,647].
+ * Complexity:
+ *
+ * expected worst-case time complexity is O(N); 
+ * expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
+ * Elements of input arrays can be modified.
+ */
+
+function solution(A) {
+    A.sort();
+    var output = 1;
+    if (A.indexOf(1) == -1) {
+        return 1;
+    }
+    for (i = A.indexOf(1); i < A.length; i++) {
+        if (A[i] != output) {
+            output++;
+            if (A[i] != output) {
+                return output;
+            }
+        }
+    }
+    return output + 1;
+}
